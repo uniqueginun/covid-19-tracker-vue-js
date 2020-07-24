13085244ed357aec1b9d81f8b51f0aff6d4d067e
Othman Mohammed <uniqueginun@gmail.com>
2020-07-25 00:08:24 +0300

    get the example map workin

--- src/components/includes/CovidMap.vue
+++ src/components/includes/CovidMap.vue
@@ -1,5 +1,98 @@
 <template>
-  <v-container>
-    <v-card class="mx-auto" outlined>map</v-card>
+  <v-container class="map__container">
+    <v-card class="mx-auto map__card" outlined>
+      <l-map
+        v-if="showMap"
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        style="height: 80%"
+        @update:center="centerUpdate"
+        @update:zoom="zoomUpdate"
+      >
+        <l-tile-layer :url="url" :attribution="attribution" />
+        <l-marker :lat-lng="withPopup">
+          <l-popup>
+            <div @click="innerClick">
+              I am a popup
+              <p v-show="showParagraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+                Donec finibus semper metus id malesuada.
+              </p>
+            </div>
+          </l-popup>
+        </l-marker>
+        <l-marker :lat-lng="withTooltip">
+          <l-tooltip :options="{ permanent: true, interactive: true }">
+            <div @click="innerClick">
+              I am a tooltip
+              <p v-show="showParagraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+                Donec finibus semper metus id malesuada.
+              </p>
+            </div>
+          </l-tooltip>
+        </l-marker>
+      </l-map>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 4,
+      center: latLng(21.3891, 39.8579),
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(21.3891, 39.8579),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(21.3891, 39.8579),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.map__container,
+.leaflet-container {
+  height: 100% !important;
+}
+.map__card {
+  height: 600px;
+}
+</style>
